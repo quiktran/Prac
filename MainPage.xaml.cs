73dc7e8dc56e8f0b96d6a43b3e7e3ce4383ee56f
quiktran <32379196+quiktran@users.noreply.github.com>
2018-09-29 11:40:27 +0700

    WSAD-II

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using JSONSample.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace JSONSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            string FilePath = Path.Combine(Package.Current.InstalledLocation.Path, "json1.json");
+
+            using (StreamReader file = File.OpenText(FilePath))
+            {
+                var json = file.ReadToEnd();
+                Dictionary<string, object> result 
+                    = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                string contacts = result["country_list"].ToString();
+                List<Country> objResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Country>>(contacts);
+                countryList.ItemsSource = objResponse;
+            }
+        }
+    }
+}
